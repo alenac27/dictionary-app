442a3c1c2d67626a91541fff2a9efac95616624d
alenac27 <124631237+alenac27@users.noreply.github.com>
2023-06-17 19:22:53 +0200

    rendered meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results.js";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data.meanings[0]);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -25,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange}></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
