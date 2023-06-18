547606421b90a3eff80ad16c96cc21eb0fe49dbc
alenac27 <124631237+alenac27@users.noreply.github.com>
2023-06-18 20:32:40 +0200

    display images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,31 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data);
+  }
+
+  function handleImagesResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let key = "d2b000c63899bdd9d62748ft3o63f2a2";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${key}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let imagesApiKey = "d2b000c63899bdd9d62748ft3o63f2a2";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleSubmit(event) {
@@ -50,6 +61,7 @@ export default function Dictionary(props) {
         </div>
 
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
